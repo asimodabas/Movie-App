a586912141da7d24008d9ae0bbb8aba56c640fce
Windowss <asimodabas03@hotmail.com>
2023-01-28 14:12:33 +0300

    HomeViewModel.kt added

--- app/src/main/java/com/asimodabas/movie_app/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/asimodabas/movie_app/ui/fragment/HomeFragment.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.asimodabas.movie_app.R
+import com.asimodabas.movie_app.adapter.MovieAdapter
+import com.asimodabas.movie_app.adapter.PopularAdapter
+import com.asimodabas.movie_app.adapter.TheatersAdapter
 import com.asimodabas.movie_app.databinding.FragmentHomeBinding
 import com.asimodabas.movie_app.util.viewBinding
 import com.asimodabas.movie_app.viewmodel.HomeViewModel
@@ -15,8 +21,72 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private val binding by viewBinding(FragmentHomeBinding::bind)
     private val viewModel: HomeViewModel by viewModels()
-
+    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var popularAdapter: PopularAdapter
+    private lateinit var theatersAdapter: TheatersAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getLiveData()
+        viewModel.getPopularLiveData()
+        viewModel.getNowPlayingLiveData()
+
+        swipeRefresh()
+        getRecycler()
+        observeLiveData()
+
+        binding.ivSearch.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToSearchFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+    private fun swipeRefresh() {
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            viewModel.refreshData()
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
+    }
+
+    private fun observeLiveData() {
+        viewModel.moviedata.observe(viewLifecycleOwner, Observer { movies ->
+            movies.let {
+                movieAdapter.updateMovieListe(movies)
+            }
+        })
+
+        viewModel.popularData.observe(viewLifecycleOwner, Observer { popular ->
+            popular.let {
+                popularAdapter.updatePopularList(popular)
+            }
+        })
+
+        viewModel.nowPlayingData.observe(viewLifecycleOwner, Observer { now ->
+            now.let {
+                theatersAdapter.updateNowList(now)
+            }
+        })
+    }
+
+    private fun getRecycler() {
+        binding.rvHighly.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        movieAdapter = MovieAdapter(arrayListOf())
+        binding.rvHighly.adapter = movieAdapter
+        binding.rvHighly.set3DItem(true)
+        binding.rvHighly.setAlpha(true)
+        binding.rvHighly.setInfinite(true)
+
+        binding.rvPopular.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        popularAdapter = PopularAdapter(arrayListOf())
+        binding.rvPopular.adapter = popularAdapter
+        binding.rvPopular.set3DItem(true)
+        binding.rvPopular.setAlpha(true)
+        binding.rvPopular.setInfinite(true)
+
+        binding.rvTheaters.layoutManager = LinearLayoutManager(context)
+        theatersAdapter = TheatersAdapter(arrayListOf())
+        binding.rvTheaters.adapter = theatersAdapter
     }
 }
--- app/src/main/java/com/asimodabas/movie_app/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/asimodabas/movie_app/viewmodel/HomeViewModel.kt
@@ -1,9 +1,17 @@
 package com.asimodabas.movie_app.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.asimodabas.movie_app.model.*
 import com.asimodabas.movie_app.service.database.DaoRepository
 import com.asimodabas.movie_app.service.retrofit.ApiServiceRepository
+import com.asimodabas.movie_app.util.CustomSharedPreferences
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observers.DisposableSingleObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,4 +21,172 @@ class HomeViewModel @Inject constructor(
     application: Application
 ) : BaseViewModel(application) {
 
+    private val compositeDisposable = CompositeDisposable()
+    private var customSharedPreferences = CustomSharedPreferences(getApplication())
+    private var refreshTime = 10 * 60 * 1000 * 1000 * 1000L
+
+    val moviedata = MutableLiveData<List<Movie>>()
+    val popularData = MutableLiveData<List<Popular>>()
+    val nowPlayingData = MutableLiveData<List<Now>>()
+
+    fun refreshData() {
+        getDataFromAPI()
+        getDataPopularFromAPI()
+        getDataNowFromAPI()
+    }
+
+    fun getLiveData() {
+        val updateTime = customSharedPreferences.getTime()
+        if (updateTime != null && updateTime != 0L && System.nanoTime() - updateTime < refreshTime) {
+            getMovieSQLite()
+        } else {
+            getDataFromAPI()
+        }
+    }
+
+    fun getPopularLiveData() {
+        val updateTime2 = customSharedPreferences.getTime()
+        if (updateTime2 != null && updateTime2 != 0L && System.nanoTime() - updateTime2 < refreshTime) {
+            getPopularSQLite()
+        } else {
+            getDataPopularFromAPI()
+        }
+    }
+
+    fun getNowPlayingLiveData() {
+        val updateTime4 = customSharedPreferences.getTime()
+        if (updateTime4 != null && updateTime4 != 0L && System.nanoTime() - updateTime4 < refreshTime) {
+            getNowSQLite()
+        } else {
+            getDataNowFromAPI()
+        }
+    }
+
+    private fun getDataFromAPI() {
+        compositeDisposable.add(
+            apiRepository.getDataService().subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<MovieResponse>() {
+                    override fun onSuccess(t: MovieResponse) {
+                        movieFromSQLite(t.results)
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                    }
+                })
+        )
+    }
+
+    private fun showMovie(movieList: List<Movie>) {
+        moviedata.value = movieList
+    }
+
+    private fun movieFromSQLite(movieList: List<Movie>) {
+        launch {
+            dbRepository.deleteMovies()
+            val listlong = dbRepository.insertAll(*movieList.toTypedArray())
+            var i = 0
+            while (i < movieList.size) {
+                movieList[i].id = listlong[i].toInt()
+                i++
+            }
+            showMovie(movieList)
+        }
+        customSharedPreferences.saveTime(System.nanoTime())
+    }
+
+    private fun getMovieSQLite() {
+        launch {
+            val movies = dbRepository.getALLMovie()
+            showMovie(movies)
+        }
+    }
+
+    private fun getDataPopularFromAPI() {
+        compositeDisposable.add(
+            apiRepository.getPopularService().subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<PopularResponse>() {
+                    override fun onSuccess(t: PopularResponse) {
+                        popularInSQLite(t.results)
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                    }
+                })
+        )
+    }
+
+    private fun showPopular(popular: List<Popular>) {
+        popularData.value = popular
+    }
+
+    private fun popularInSQLite(popular: List<Popular>) {
+        launch {
+            dbRepository.deletePopulars()
+            val listlong = dbRepository.insertAllPopular(*popular.toTypedArray())
+            var i = 0
+            while (i < popular.size) {
+                popular[i].id = listlong[i].toInt()
+                i++
+            }
+            showPopular(popular)
+        }
+        customSharedPreferences.saveTime(System.nanoTime())
+    }
+
+    private fun getPopularSQLite() {
+        launch {
+            val populars = dbRepository.getAllPopular()
+            showPopular(populars)
+        }
+    }
+
+    private fun getDataNowFromAPI() {
+        compositeDisposable.add(
+            apiRepository.getNowPlayingService().subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<NowResult>() {
+                    override fun onSuccess(t: NowResult) {
+                        nowInSQLite(t.results)
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                    }
+                })
+        )
+    }
+
+    private fun showNow(now: List<Now>) {
+        nowPlayingData.value = now
+    }
+
+    private fun nowInSQLite(now: List<Now>) {
+        launch {
+            dbRepository.deleteNow()
+            val listlong = dbRepository.insertAllNow(*now.toTypedArray())
+            var i = 0
+            while (i < now.size) {
+                now[i].id = listlong[i].toInt()
+                i++
+            }
+            showNow(now)
+        }
+        customSharedPreferences.saveTime(System.nanoTime())
+    }
+
+    private fun getNowSQLite() {
+        launch {
+            val now = dbRepository.getAllNow()
+            showNow(now)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
 }
